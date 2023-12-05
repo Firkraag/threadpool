fd37a2c07daa60bb718b83db3c67d96f3938fdd0
Firkraag <cntqrxj@gmail.com>
2023-12-05 12:52:04 +0800

    don't assign fixed value to enum

--- threadpool.h
+++ threadpool.h
@@ -1,61 +1,56 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
+#include "list.h"
 #include "memory"
 #include "pthread.h"
-#include "list.h"
 
 class threadpool;
 
 typedef void *(*fork_join_task_t)(threadpool *pool, void *data);
 
-typedef enum {
-    NOT_STARTED = 0,
-    IN_PROGRESS = 1,
-    COMPLETED = 2
-} status_t;
+typedef enum { NOT_STARTED, IN_PROGRESS, COMPLETED } status_t;
 
 class future : public list_element {
 public:
-    status_t status = NOT_STARTED;
-    void *data;
-    fork_join_task_t task;
-    void *result;
-    threadpool *pool;
-    pthread_cond_t done;
+  status_t status = NOT_STARTED;
+  void *data;
+  fork_join_task_t task;
+  void *result;
+  threadpool *pool;
+  pthread_cond_t done;
 
-    void *get();
+  void *get();
 
-    future(void *data, fork_join_task_t task, threadpool *pool);
+  future(void *data, fork_join_task_t task, threadpool *pool);
 
-    ~future();
+  ~future();
 };
 
 class worker {
 public:
-    pthread_t tid;
+  pthread_t tid;
 
-    void run(threadpool *pool);
+  void run(threadpool *pool);
 
-    ~worker();
+  ~worker();
 };
-
 
 class threadpool {
 private:
-    worker *workers;
+  worker *workers;
+
 public:
-    list global_queue;
-    bool shutdown = false;
-    pthread_mutex_t lock;
-    pthread_cond_t has_task;
+  list global_queue;
+  bool shutdown = false;
+  pthread_mutex_t lock;
+  pthread_cond_t has_task;
 
-    explicit threadpool(int nthreads);
+  explicit threadpool(int nthreads);
 
-    std::unique_ptr<future> submit(fork_join_task_t task, void *data);
+  std::unique_ptr<future> submit(fork_join_task_t task, void *data);
 
-    ~threadpool();
+  ~threadpool();
 };
-
 
 #endif
